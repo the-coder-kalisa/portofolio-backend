212195a96eee27c6a8b488e92bedc579a16f273c
the-coder-kalisa <gthecoderkalisaineza@gmail.com>
2022-08-12 17:04:57 +0200

    finished

--- server.js
+++ server.js
@@ -2,19 +2,33 @@ const express = require("express");
 const app = express();
 const { validateEmail, mail, transporter } = require("./validator");
 require("dotenv").config();
+const cors = require("cors");
 const http = require("http");
+const emailExistence = require("email-existence");
 const PORT = 5000;
+app.use(express.json()).use(express.urlencoded());
+app.use(cors());
 app.get("/", (_req, res) => {
   res.send("backend for my portfolio");
 });
 app.post("/", (req, res) => {
   const { email, message } = req.body;
+
   if (validateEmail(email)) {
-    transporter.sendMail(mail(message, email), (err, info) => {
-      if (err) {
-        res.status(500).send(err.message);
+    emailExistence.check(email, (err, response) => {
+      if (err) return res.status(500).send("something went wrong");
+      if (response) {
+        transporter.sendMail(mail(message, email), (err, info) => {
+          if (err) {
+            console.log(err);
+            res.status(500).send("something went wrong");
+          } else {
+            console.log(info);
+            res.send("email sent");
+          }
+        });
       } else {
-        res.status(200).send(info.response);
+        res.status(404).send("email not found");
       }
     });
   } else {
@@ -23,4 +37,4 @@ app.post("/", (req, res) => {
 });
 
 const server = http.createServer(app);
-server.listen(PORT, () => console.log(`server running on port ${PORT}`))
+server.listen(PORT, () => console.log(`server running on port ${PORT}`));
--- validator.js
+++ validator.js
@@ -4,16 +4,8 @@ const nodemailer = require("nodemailer")
 exports.validateEmail = (email) => {
   const schema = joi.string().email().required();
   const { error } = schema.validate(email);
-  if (!error) {
-    return emailExistence.check(
-      email,
-      (err, response) => {
-        return response || !err ? true : false;
-      }
-    );
-  } else {
-    return false;
-  }
+  if(error) return false;
+    return true;
 };
 
 exports.transporter = nodemailer.createTransport({
