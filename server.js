6c2230cc1fc5d4b2743057763b067ad001598cda
the-coder-kalisa <gthecoderkalisaineza@gmail.com>
2022-09-25 17:13:43 +0200

    wow

--- server.js
+++ server.js
@@ -1,39 +1,48 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
-const { validateEmail, mail, transporter } = require("./validator");
-require("dotenv").config();
 const cors = require("cors");
 const emailExistence = require("email-existence");
-app.use(express.json()).use(express.urlencoded());
-app.use(cors());
-app.get("/", (_req, res) => {
-  res.send("backend for my portfolio");
+const nodemailer = require("nodemailer");
+app.use(express.json()).use(express.urlencoded()).use(cors());
+const port = process.env.PORT || 5000;
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  auth: {
+    type: "OAuth2",
+    user: process.env.GMAIL_ADDRESS,
+    clientId: process.env.GMAIL_OAUTH_CLIENT_ID,
+    clientSecret: process.env.GMAIL_OAUTH_CLIENT_SECRET,
+    refreshToken: process.env.GMAIL_OAUTH_REFRESH_TOKEN,
+    accessToken: process.env.GMAIL_OAUTH_ACCESS_TOKEN,
+    expires: Number.parseInt(process.env.GMAIL_OAUTH_TOKEN_EXPIRE, 10),
+  },
 });
-var port = process.env.PORT || 5000;
-app.post("/", (req, res) => {
-  const { email, message, name } = req.body;
-
-  if (validateEmail(email)) {
-    emailExistence.check(email, (err, response) => {
-      if (err) return res.status(500).send("something went wrong");
+app.post("/send", (req, res) => {
+  const { name, email, message } = req.body;
+  const mailOptions = {
+    from: email,
+    to: process.env.GMAIL_ADDRESS,
+    subject: `hiring`,
+    text: `name: ${name} email: ${email} message: ${message}`,
+  };
+  if (
+    emailExistence.check(email, (error, response) => {
+      if (error) return res.status(500).send(error);
       if (response) {
-        transporter.sendMail(mail(message, email, name), (err, info) => {
+        transporter.sendMail(mailOptions, (err, data) => {
           if (err) {
-            console.log(err);
             res.status(500).send("something went wrong");
           } else {
-            console.log(info);
-            res.send("email sent");
+            res.status(200).json("Email sent successfully");
           }
         });
       } else {
-        res.status(404).send("email not found");
+        res.status(404).send("email does not exist");
       }
-    });
-  } else {
-    res.status(400).send("Invalid email");
-  }
+    })
+  );
 });
-app.listen(port, () => {
-  console.log("localhost:" + port);
-});
+app.listen(port, () => {console.log(`localhost:${port}`)})
